139b95108ead04f2f5dc719b44c5c28eaa883b2a
muran001 <macmuran001@gmail.com>
2014-05-17 05:24:15 +0900

    add 404/500 page if routing does not match anything

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,32 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from Exception, with: :render_500
+
+  def render_404( exception = nil)
+    if exception
+      logger.info "Rendering 404 with exception: #{exception.message}"
+    end
+
+    render template: 'error/error_404', 
+           status: 404, 
+           layout: 'application', 
+           content_type: 'text/html'
+  end
+
+  def render_500( exception = nil )
+    if exception    
+      logger.info "Rendering 500 with exception: #{exception.message}"
+    end
+
+    render template: "errors/error_500", 
+           status: 500, 
+           layout: 'application'
+
+
+  end
+
 end
